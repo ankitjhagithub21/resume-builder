359a90fef1ffdb3e9e326c467d50bae75005c328
ankitjhagithub21 <ajha45075@gmail.com>
2025-06-22 09:37:26 +0530

    created user modal

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const connectDB = require('./db/conn')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
 connectDB()
 
 app.use(express.json())
+app.use(cors({
+  origin:process.env.ORIGIN,
+  credentials:true
+}))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
